929a5f9b46337cc534db1bc0f65eb5197f795a87
Lucas Alves Barbosa <43991255+gibibigi@users.noreply.github.com>
2023-10-25 21:11:52 -0300

    a
    a

---
+++ exercicios/ex014.js
@@ -0,0 +1,15 @@
+var i = 1;
+/*do{
+    console.log('Ai chaves, ai chavinho!' + i);
+    i++;
+}while(i <= 6)
+
+while(i <=6){
+    console.log('Ai chaves, ai chavinho!' + i);
+    i++;
+}*/
+console.log('Vai começar...');
+for(var i=0;i<=10;i++){
+    console.log(i);
+}
+console.log('FIM!');
